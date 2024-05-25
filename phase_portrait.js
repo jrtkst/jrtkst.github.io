ae961f9774dc1499831ebd99450cfd842ae54cb3
jrtkst <jrtkst@gmail.com>
2024-05-25 19:06:58 +0700

    t

--- phase_portrait.js
+++ phase_portrait.js
@@ -1,11 +1,11 @@
 function generate() {
-    console.log('masuk');
+    
     var quad = document.querySelector('#quad').checked;
     var lx = Number(document.getElementById('lx').value);
     var ly = Number(document.getElementById('ly').value);
     var narrows = Number(document.getElementById('narrows').value);
     var inits = document.getElementById('inits').value;
-    var inits_arr = inits.replaceAll("(", '').replaceAll(")", '').split(",")
+    var inits_arr = inits.replaceAll("(", '').replaceAll(")", '').split(",");
 
 
 
@@ -14,9 +14,12 @@ function generate() {
     const yb = ly;
     const xa = quad ? -lx : 0;
     const ya = quad ? -ly : 0;        
-    X = linspace(xa, xb, narrows)
-    Y = linspace(ya, yb, narrows)
-    console.log(X)
+    X = linspace(xa, xb, narrows);
+    Y = linspace(ya, yb, narrows);
+    
+
+    // Check apakah odeint working
+    
 
 
 }
@@ -30,10 +33,10 @@ function linspace(start, stop, num) {
 }   
 
 function odeint(funcs, Z0, T, num=200) {
-    "Solving ODE with Runge-Kutta algorithm."
+    "ODE integration with Runge-Kutta algorithm"
     var fs = funcs();
     var k = length(fs); // Number of functions
-    var h = T/num;
+    var h = T/num;      // Timestep
     var tspan = linspace(0, T, num+1);
     var out = Array(k + 1).fill().map(() => Array(num + 1).fill(0));
     for (var i = 0; i < num + 1; i++) {
@@ -47,29 +50,51 @@ function odeint(funcs, Z0, T, num=200) {
     for (var i = 1; i < num + 1; i++) {
         // K1
         var K1 = [];
-        var inp1 = tspan[i - 1].concat(sol_i);
-        for (var j = 0; j < k; i++) {
-            var K1 = K1.concat(fs[j](inp1).map((x, _) => x*h));
+        var inp1 = [tspan[i - 1]].concat(sol_i);
+        for (var j = 0; j < k; j++) {
+            var K1 = K1.concat(fs[j](inp1).map((x, _) => h * x));
         };
         // K2
         var K2 = [];
-        
-
+        var inp2 = sumArr(inp1, kArr(0.5, [h].concat(K1)));
+        for (var j = 0; j < k; j++) { 
+            var K2 = K2.concat(fs[j](inp2).map((x, _) => h * x)) 
+        };
         // K3
         var K3 = [];
-
+        var inp3 = sumArr(inp1, kArr(0.5, [h].concat(K2)));
+        for (var j = 0; j < k; j++) { 
+            var K3 = K3.concat(fs[j](inp3).map((x, _) => h * x)) 
+        };
         // K4
         var K4 = [];
-
-        var sol_i = sol_i + ...
-
+        var inp4 = sumArr(inp1, [h].concat(K3));
+        for (var j = 0; j < k; j++) { 
+            var K4 = K4.concat(fs[j](inp4).map((x, _) => h * x)) 
+        };
+        var sol_i = sumArr(sol_i, kArr(1/6, sumArr(K1, kArr(2, K2), kArr(2, K3), K4)));
+        for (var s = 1; s < k + 1; s++) {
+            out[s][i] = sol_i[s];
+        }
     };
+    return out;
 };
-function sumArr(a, b) {
-    return b.map((val, i) => val + a[i]);
+function sumArr(...arr) {
+    var n = arr.length;     // Number of arrays
+    var k = arr[0].length;  // Entries of arrays
+    var out = Array(arr[0].length).fill(0);
+    for (var i = 0; i < n ;i++) {
+        for (var j = 0; j < k; j++) {
+            out[j] += arr[i][j]; 
+        };
+    };
+    return out;
 };
-function mulArr() {
-
+function kArr(k, arr) {
+    return arr.map((val, _) => val * k);
+};
+function mulArr(a, b) {
+    return a.map((val, i) => val * b[i]);
 };
 function meshgrid() {
 
